4662c4266582e467fda566db0ca8b2ee666a4fa0
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-18 09:44:03 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cbfv
+admin.site.register(Cbfv)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Cbfv(models.Model):
+    'Generated Model'
+    cvbvn = models.BigIntegerField()
+    erty = models.BigIntegerField()
